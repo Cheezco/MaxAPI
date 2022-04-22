3cfd1817d3fb8022b009d775ef08e01e0cbc6452
Cheezco <60855273+Cheezco@users.noreply.github.com>
2022-04-22 17:55:07 +0300

    Prisijungimas

---
+++ MaxAPI/Controllers/Account/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using MaxAPI.Contexts;
+using MaxAPI.Models;
+using MaxAPI.Models.Accounts;
+using MaxAPI.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaxAPI.Controllers.Account
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly MainContext _context;
+
+        public LoginController(MainContext context)
+        {
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<ActionResult<User>> Login(LoginUser loginUser)
+        {
+            var user = await AuthenticationUtils.AuthenticateUserAsync(_context, loginUser);
+
+            if (user is null) return Unauthorized();
+
+            return Ok(new { id = user.Id, username = user.Username, email = user.Email, token = AuthenticationUtils.GenerateJSONWebToken(user) });
+        }
+    }
+}
